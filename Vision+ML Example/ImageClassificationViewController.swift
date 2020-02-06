1f49950d5b3f7aca2e223ea480145557444293eb
Lionel Jiang <lionel@eventxtra.com>
2020-02-06 16:34:16 +0800

    initial commit

---
+++ Vision+ML Example/ImageClassificationViewController.swift
@@ -0,0 +1,150 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+View controller for selecting images and applying Vision + Core ML processing.
+*/
+
+import UIKit
+import CoreML
+import Vision
+import ImageIO
+
+class ImageClassificationViewController: UIViewController {
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
+    // MARK: - Image Classification
+    
+    /// - Tag: MLModelSetup
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            /*
+             Use the Swift class `MobileNet` Core ML generates from the model.
+             To use a different Core ML classifier model, add it to the project
+             and replace `MobileNet` with that model's generated Swift class.
+             */
+            let model = try VNCoreMLModel(for: AutoML().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    /// - Tag: PerformRequests
+    func updateClassifications(for image: UIImage) {
+        classificationLabel.text = "Classifying..."
+        
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+        func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+        
+            if classifications.isEmpty {
+                self.classificationLabel.text = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(2)
+                var className = ""
+                switch topClassifications[0].identifier {
+                    case "CICAgICAwKyxWBIHZ29uZG9sYQ==":
+                      className = "gondola"
+                    case "CICAgICAwKyxGBIKZmVycnkgYm9hdA==":
+                      className = "ferry boat"
+                    case "CICAgICAwKyxcBIMZnJlaWdodCBib2F0":
+                      className = "freight boat"
+                    case "CICAgICAwKyxUBILY3J1aXNlIHNoaXA=":
+                      className = "cruise ship"
+                    case "CICAgICAwKyxCBIEYnVveQ==":
+                      className = "buoy"
+                    case "CICAgICAwKyxMBIIc2FpbGJvYXQ=":
+                      className = "sailboat"
+                    case "CICAgICAwKyxSBIPaW5mbGF0YWJsZSBib2F0":
+                      className = "inflatable "
+                    case "CICAgICAwKyxKBIKcGFwZXIgYm9hdA==":
+                      className = "paper boat"
+                    default:
+                      className = "Nothing recognized."
+                }
+                let descriptions = String(format: "  (%.2f) %@", topClassifications[0].confidence, className)
+                self.classificationLabel.text = "Classification:\n" + descriptions
+            }
+        }
+    }
+    
+    // MARK: - Photo Actions
+    
+    @IBAction func takePicture() {
+        // Show options for the source picker only if the camera is available.
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        let photoSourcePicker = UIAlertController()
+        let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .camera)
+        }
+        let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        
+        photoSourcePicker.addAction(takePhoto)
+        photoSourcePicker.addAction(choosePhoto)
+        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(photoSourcePicker, animated: true)
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerControllerSourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
+}
+
+extension ImageClassificationViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: - Handling Image Picker Selection
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        picker.dismiss(animated: true)
+        
+        // We always expect `imagePickerController(:didFinishPickingMediaWithInfo:)` to supply the original image.
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        updateClassifications(for: image)
+    }
+}
